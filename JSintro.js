b8d7eebb5c1c5397f9940f4490f70d9a80d1a7e2
miritzelichonok1 <miritz@startapp.com>
2021-11-17 21:39:41 +0200

    lecture 5

---
+++ JSintro.js
@@ -0,0 +1,17 @@
+var x, y, z;
+
+x=7; y=5; z=x+y;
+console.log(z); // console
+
+function click() {
+    document.getElementById('demo').innerHTML = "Hello JS function!";
+}
+
+var user = {
+    first_name: "eli", 
+    last_name: "cohen", 
+    email: "elicohen@walla.com", 
+    age:25
+}
+
+consloe.log(user);
